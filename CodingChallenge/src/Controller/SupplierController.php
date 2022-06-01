4bc0f5788326141a73585c8cbf232d155ff44aa6
projecto3m12 <102583626+projecto3m12@users.noreply.github.com>
2022-06-02 00:52:31 +0200

    Añadir un proveedor

--- CodingChallenge/src/Controller/SupplierController.php
+++ CodingChallenge/src/Controller/SupplierController.php
@@ -2,19 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Supplier;
+use App\Form\SupplierType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SupplierController extends AbstractController
 {
     /**
-     * @Route("/supplier", name="app_supplier")
+     * @Route("/", name="app_supplier")
      */
     public function index(): Response
     {
         return $this->render('supplier/index.html.twig', [
             'controller_name' => 'SupplierController',
+        ]);
+    }
+
+    /**
+     * @Route("create", name="create")
+     */
+    public function create(Request $request) {
+        $supplier = new Supplier();
+        $form = $this->createForm(SupplierType::class, $supplier);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $userExists = $em->getRepository(Supplier::class)->findBy(['mail'=>$supplier->getMail()]);
+
+            if ($userExists == null) {
+                $em->persist($supplier);
+                $em->flush();
+                return $this->redirectToRoute('app_supplier');
+            } else {
+                $this->addFlash('notice','Este proveedor ya ha sido registrado previamente!');
+            }
+        }
+
+        return $this->render('supplier/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
