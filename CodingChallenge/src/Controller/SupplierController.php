144b508861416e82a9dffe07e219bcc938508fd3
projecto3m12 <102583626+projecto3m12@users.noreply.github.com>
2022-06-02 02:13:16 +0200

    Listar proveedores y Editar un proveedor
    Dos pasos seguidos

--- CodingChallenge/src/Controller/SupplierController.php
+++ CodingChallenge/src/Controller/SupplierController.php
@@ -16,8 +16,13 @@ class SupplierController extends AbstractController
      */
     public function index(): Response
     {
+        $data = $this->getDoctrine()->getRepository(Supplier::class)->findAll();
+
+
+
+
         return $this->render('supplier/index.html.twig', [
-            'controller_name' => 'SupplierController',
+            'suppliers' => $data
         ]);
     }
 
@@ -45,5 +50,42 @@ class SupplierController extends AbstractController
         return $this->render('supplier/create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update(Request $request, $id) {
+        $supplier = $this->getDoctrine()->getRepository(Supplier::class)->find($id);
+        $form = $this->createForm(SupplierType::class, $supplier);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $userExists = $em->getRepository(Supplier::class)->findBy(['mail'=>$supplier->getMail()]);
+
+            if ($userExists != null) {
+                if ($userExists[0]->getId() == $supplier->getId()) {
+                    $em->persist($supplier);
+                    $em->flush();
+                    return $this->redirectToRoute('app_supplier');
+                } else {
+                    $this->addFlash('notice','Este correo pertenece a otro empleado!');
+                }
+            } else {
+                $this->addFlash('notice','Este proveedor ya ha sido registrado previamente!');
+            }
+        }
+
+        return $this->render('supplier/update.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Request $request, $id) {
+        
     }
 }
